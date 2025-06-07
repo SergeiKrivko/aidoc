9cdca85da6bde39f8f64f3fde42ac11f5d38b4ea
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-06-07 18:42:54 +0300

    add cli

--- AiDoc.Git/GitClient.cs
+++ AiDoc.Git/GitClient.cs
@@ -7,10 +7,13 @@ public class GitClient
 {
     public static IEnumerable<ModifiedSourceFile> GetStructureDiff(string repoPath, string commitSha)
     {
-        using (var repo = new Repository(repoPath))
+        using (var repo = FindRepo(repoPath))
         {
+            if (repo is null)
+                throw new Exception("Repo not found");
+
             var commit = repo.Lookup<Commit>(commitSha);
-            if (commit == null)
+            if (commit is null)
                 throw new Exception("Commit not found");
 
             // Получаем разницу между текущим состоянием и указанным коммитом
@@ -29,15 +32,47 @@ public class GitClient
 
     public static string GetFileDiff(string repoPath, string filePath, string commitSha)
     {
-        using (var repo = new Repository(repoPath))
+        using (var repo = FindRepo(repoPath))
         {
+            if (repo is null)
+                throw new Exception("Repo not found");
+
             var commit = repo.Lookup<Commit>(commitSha);
-            if (commit == null)
+            if (commit is null)
                 throw new Exception("Commit not found");
 
             // Получаем разницу между текущим состоянием и указанным коммитом
             var diff = repo.Diff.Compare<Patch>(commit.Tree, repo.Head.Tip.Tree, [filePath]);
             return diff.Content;
         }
+    }
+
+    public static IEnumerable<string> GetIgnoredFiles(string repoPath)
+    {
+        using (var repo = FindRepo(repoPath))
+        {
+            if (repo is null)
+                throw new Exception("Repo not found");
+
+            var ignoredFiles = new List<string>();
+
+            foreach (var entry in Directory.GetFiles(repo.Info.Path, "*.*", SearchOption.AllDirectories))
+            {
+                var relativePath = Path.GetRelativePath(repoPath, entry);
+                if (repo.Ignore.IsPathIgnored(relativePath))
+                {
+                    ignoredFiles.Add(entry);
+                }
+            }
+            
+            return ignoredFiles;
+        }
+    }
+
+    private static Repository? FindRepo(string repoPath)
+    {
+        var foundRepoPath = Repository.Discover(repoPath);
+
+        return foundRepoPath is null ? null : new Repository(foundRepoPath);
     }
 }
