19e163f6460522fd760350203df0ffce5a46eb73
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-06-07 23:00:17 +0300

    add features

--- api/app/api/routers/agent_router.py
+++ api/app/api/routers/agent_router.py
@@ -1,6 +1,4 @@
-from typing import Optional
-
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter
 
 from app.api import schemas
 from app.services.agent_svc import OpenAISvcDep
@@ -9,22 +7,39 @@ router = APIRouter()
 
 
 @router.post(path="/init", response_model=schemas.AgentResponseModel)
-async def agent_request(
+async def init_handler(
     ai_agent: OpenAISvcDep,
-    agent_request: schemas.InitRequest = Body(...),
+    agent_request: schemas.InitRequest,
 ) -> schemas.AgentResponseModel:
-
-    result = await ai_agent.request(agent_request=agent_request, user_message=str(agent_request.model_dump()))
-
+    result = await ai_agent.request(
+        agent_request=agent_request,
+        user_message=agent_request.model_dump_json(),
+    )
     return result
 
 
 @router.post(path="/request", response_model=schemas.AgentResponseModel)
-async def agent_request(
+async def request_handler(
     ai_agent: OpenAISvcDep,
-    agent_request: schemas.AgentRequestModel = Body(...)
-    ) -> schemas.AgentResponseModel:
-    
+    agent_request: schemas.AgentRequestModel,
+) -> schemas.AgentResponseModel:
     result = await ai_agent.request(agent_request=agent_request)
-    
+    return result
+
+
+@router.post(path="/features/init", response_model=schemas.AgentResponseModel)
+async def features_init_handler(
+    ai_agent: OpenAISvcDep,
+    features_init: schemas.FeaturesInitRequest,
+) -> schemas.AgentResponseModel:
+    result = await ai_agent.features_init(features_init)
+    return result
+
+
+@router.post(path="/features/request", response_model=schemas.AgentResponseModel)
+async def features_request_handler(
+    ai_agent: OpenAISvcDep,
+    agent_request: schemas.AgentRequestModel,
+) -> schemas.AgentResponseModel:
+    result = await ai_agent.features_request(agent_request)
     return result
