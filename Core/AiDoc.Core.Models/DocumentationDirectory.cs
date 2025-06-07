dcc5432a0d149150f501e8922053e53fb22cb28f
SergeiKrivko <sergik.krivko@gmail.com>
2025-06-07 14:52:16 +0300

    Models

---
+++ Core/AiDoc.Core.Models/DocumentationDirectory.cs
@@ -0,0 +1,9 @@
+﻿namespace AiDoc.Core.Models;
+
+public class DocumentationDirectory : IDocumentationNode
+{
+    public required string Path { get; set; }
+    public required string Label  { get; set; }
+    public required string Description  { get; set; }
+    public int Position { get; set; }
+}
---
+++ Core/AiDoc.Core.Models/IDocumentationNode.cs
@@ -0,0 +1,7 @@
+﻿namespace AiDoc.Core.Models;
+
+public interface IDocumentationNode
+{
+    public string Path { get; set; }
+    public int Position { get; set; }
+}
