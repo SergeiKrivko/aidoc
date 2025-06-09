5faf8463f1994660b668d0eee9a17d725f28a9a3
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-06-09 12:49:52 +0300

    remove hardcoded github repo

--- api/app/api/schemas/info.py
+++ api/app/api/schemas/info.py
@@ -1,6 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 
 class Info(BaseModel):
     name: str
     github_repo: str
+
+    def github_repo_name(self) -> str:
+        """
+        Получаем название репы из ссылки.
+        По-хорошему, надо сделать так, чтобы github_repo изначально был HttpUrl.
+        Но это позже.
+        :return:
+        """
+        try:
+            repo_url = HttpUrl(self.github_repo)
+            path_segments = [s for s in repo_url.path.split("/") if s]
+            if not path_segments:
+                raise ValueError
+        except ValueError:
+            print(f"invalid github_repo: {self.github_repo}")
+            return self.name
+        else:
+            return path_segments[-1]
